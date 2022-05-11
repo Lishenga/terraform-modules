652958edc8c77d8bf89cb2b2bfd0987efb6738ab
leon lishenga <lishengaleon@gmail.com>
2022-05-11 10:39:30 +0300

    AMI

--- modules/ecs-cluster/securitygroups.tf
+++ modules/ecs-cluster/securitygroups.tf
@@ -1,4 +1,4 @@
-/* resource "aws_security_group" "cluster" {
+resource "aws_security_group" "cluster" {
   name        = var.cluster_name
   vpc_id      = var.vpc_id
   description = var.cluster_name
@@ -21,9 +21,9 @@ resource "aws_security_group_rule" "cluster-egress" {
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
-} */
+}
 
-resource "aws_security_group" "cluster" {
+/* resource "aws_security_group" "cluster" {
   name        = var.cluster_name
   vpc_id      = var.vpc_id
   description = var.cluster_name
@@ -71,5 +71,5 @@ resource "aws_security_group" "cluster-securitygroup" {
   tags = {
     Name = "cluster-app-elb-security-group"
   }
-}
+} */
 
