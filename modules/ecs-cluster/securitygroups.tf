1c42a010a1b8f99c1f4c8c41703e95f4c14bca55
leon lishenga <lishengaleon@gmail.com>
2022-05-27 09:18:16 +0300

    changes

--- modules/ecs-cluster/securitygroups.tf
+++ modules/ecs-cluster/securitygroups.tf
@@ -9,7 +9,7 @@ resource "aws_security_group_rule" "cluster-allow-ssh" {
   security_group_id        = aws_security_group.cluster.id
   type                     = "ingress"
   from_port                = 22
-  to_port                  = 22
+  to_port                  = 9100
   protocol                 = "tcp"
   cidr_blocks              = ["0.0.0.0/0"]
   source_security_group_id = var.ssh_sg
