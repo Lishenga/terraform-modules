b67f30621d2fab9d2e1ebe6a57647d924179f37e
leon lishenga <lishengaleon@gmail.com>
2022-08-15 11:48:56 +0300

    push

--- modules/ecs-cluster/securitygroups.tf
+++ modules/ecs-cluster/securitygroups.tf
@@ -60,6 +60,15 @@ resource "aws_security_group_rule" "event-microservice-port" {
   source_security_group_id = var.ssh_sg
 }
 
+resource "aws_security_group_rule" "event-microservice-port" {
+  security_group_id        = aws_security_group.cluster.id
+  type                     = "ingress"
+  from_port                = 3100
+  to_port                  = 3100
+  protocol                 = "tcp"
+  source_security_group_id = var.ssh_sg
+}
+
 /*
 resource "aws_security_group_rule" "cluster-loki-promtail" {
   security_group_id        = aws_security_group.cluster.id
